34f5eb5216e5849a8481eff07a97879a87e74691
phg-2025 <horst-guenther@t-online.de>
2025-10-31 18:45:02 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,16 @@ function Blinken (x: number, y: number) {
     led.toggle(x, y)
     basic.pause(500)
 }
-basic.forever(function () {
-	
-})
+let Bauer = 0
+let Kohl = 0
+let Wolf = 0
+let Ziege = 0
+let Aktuell = 0
+led.plot(0, 0)
+led.plot(0, 2)
+led.plot(0, 3)
+led.plot(0, 4)
 basic.forever(function () {
     Blinken(1, 1)
-    music.playTone(262, music.beat(BeatFraction.Whole))
     Blinken(2, 2)
 })
