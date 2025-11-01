dcb76655d4c388680231f312f8d2f0511339de5d
phg-2025 <horst-guenther@t-online.de>
2025-11-01 18:07:19 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,52 @@ function zeigeWolf () {
 }
 function zeigeZiege () {
     Blinken(Ziege, 4)
+    Kohl += 1
 }
 function zeigeBauer () {
     if (Aktuell == 0) {
         Blinken(Bauer, 0)
     } else {
-    	
+        input.onButtonEvent(Button.A, input.buttonEventClick(), function() {
+            
+        })
     }
 }
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    if (Aktuell == 0) {
+        Bauer += 1
+        if (Bauer == 1) {
+            Bauer += 1
+            Bauer = Bauer % 5
+        }
+    } else {
+        if (Aktuell == 2) {
+            Kohl += 1
+            if (Kohl == 1) {
+                Kohl += 1
+                Kohl = Kohl % 5
+            }
+        } else {
+            if (Aktuell == 3) {
+                Wolf += 1
+                if (Wolf == 1) {
+                    Wolf += 1
+                    Wolf = Wolf % 5
+                }
+            } else {
+                if (Aktuell == 4) {
+                    Ziege += 1
+                    if (Ziege == 1) {
+                        Ziege += 1
+                        Ziege = Ziege % 5
+                    }
+                } else {
+                	
+                }
+            }
+        }
+    }
+})
 // Komm
 function Blinken (x: number, y: number) {
     led.plot(x, y)
@@ -35,6 +73,7 @@ led.plot(0, 0)
 led.plot(0, 2)
 led.plot(0, 3)
 led.plot(0, 4)
+let objekte = [0, 5]
 basic.forever(function () {
     zeigeBauer()
     zeigeKohl()
