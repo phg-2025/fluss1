d842c785b0709c3b527abf059c5e48b5012462c8
phg-2025 <horst-guenther@t-online.de>
2025-11-01 14:11:10 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+function zeigeWolf () {
+    Blinken(Wolf, 0)
+}
+function zeigeZiege () {
+    Blinken(Ziege, 0)
+}
+function zeigeBauer () {
+    Blinken(Bauer, 0)
+}
 // Komm
 function Blinken (x: number, y: number) {
     led.plot(x, y)
@@ -5,10 +14,17 @@ function Blinken (x: number, y: number) {
     led.toggle(x, y)
     basic.pause(500)
 }
-let Bauer = 0
-let Kohl = 0
-let Wolf = 0
+function zeigeKohl () {
+    Blinken(Kohl, 0)
+}
 let Ziege = 0
+let Wolf = 0
+let Kohl = 0
+let Bauer = 0
+Bauer = 0
+Kohl = 0
+Wolf = 0
+Ziege = 0
 let Aktuell = 0
 led.plot(0, 0)
 led.plot(0, 2)
@@ -16,4 +32,7 @@ led.plot(0, 3)
 led.plot(0, 4)
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    zeigeBauer()
 })
