af04804ac4b3493a085a16a33ee3be0fc44dfd7a
phg-2025 <horst-guenther@t-online.de>
2025-11-04 16:58:30 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,9 +18,10 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     }
     Aktuell = Aktuell % 5
     led.plot(2, Aktuell)
+    oledssd1306.clearDisplay()
     oledssd1306.setTextXY(0, 0)
     oledssd1306.writeString("Aktuell")
-    oledssd1306.setTextXY(1, 0)
+    oledssd1306.setTextXY(0, 9)
     oledssd1306.writeNumber(Aktuell)
 })
 function Zeigen () {
